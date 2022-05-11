9b08e1fa14f7c87236deefededdaa158d0a76f68
Ryan Pablo Silva Araújo <83867730+RyanAraujoo@users.noreply.github.com>
2022-05-11 16:13:07 -0300

    fork

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".hamburguer").addEventListener("click", () =>
+  document.querySelector(".menu-link").classList.toggle("show-menu")
+)
+
+
+  // Essa função pega o elemento side-bar e se tiver a classe show-menu ele tira
+    // se não ele põem.
