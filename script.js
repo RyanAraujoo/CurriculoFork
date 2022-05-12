f81a4203361a3419e4ef6525e7b081378bb5c994
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-05-12 17:39:32 -0300

    update script

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-document.querySelector(".hamburguer").addEventListener("click", () =>
-  document.querySelector(".menu-link").classList.toggle("show-menu")
-)
-
+//document.querySelector(".hamburguer").addEventListener("click", () => {document.querySelectorAll(".container").classList.toggle("show-menu")} 
+//)
+const optionsMenu = document.querySelector(".sidebar")
+const btnMenu = document.getElementById("menu")
+btnMenu.addEventListener("click", () => {
+        //console.log("cliquei no menu")
+        if (optionsMenu.style.display == 'none' || optionsMenu.style.display == '') {
+          optionsMenu.style.display = 'block'
+         
+     } else {
+          optionsMenu.style.display = 'none'
+          
+  } 
+})
 
   // Essa função pega o elemento side-bar e se tiver a classe show-menu ele tira
     // se não ele põem.
